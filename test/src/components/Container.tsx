f306a1db15b02af8419c4eb4ec227cf496905bb1
Ho Nyeong <dev.honing@gmail.com>
2023-12-10 19:50:23 +0900

    항목별로 정리하고 스타일링 개선

--- test/src/components/Container.tsx
+++ test/src/components/Container.tsx
@@ -6,26 +6,51 @@ import './Container.css';
 const Container: React.FC = () => {
   return (
     <div className="Container">
+      <h1>HOW IS IT GOING?</h1>
+      <hr />
+      <div className="content">
       <span>
-      <h1>Learned</h1>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>Node.js</li>
-        <li>Express.js</li>
+      <h2>Learned</h2>
+        <ul>Basic Web
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+        <ul>Server
+          <li>Node.js</li>
+          <li>Express.js</li>
+        </ul>
       </span>
       <span>
-      <h1>Learning</h1>
-        <li>React.js</li>
-        <li>Python</li>
-        <li>MySQL</li>
+      <h2>Learning</h2>
+        <ul>Language
+          <li>Python</li>
+          <li>TypeScript</li>
+        </ul>
+        <ul>Database
+          <li>MySQL</li>
+        </ul>
+        <ul>Library
+          <li>React.js</li>
+        </ul>
       </span>
       <span>
-        <h1>Expected</h1>
-        <li>MongoDB</li>
-        <li>AI</li>
+        <h2>Expected</h2>
+        <ul>Language
+          <li>C++</li>
+          
+        </ul>
+        <ul>Database
+          <li>MongoDB</li>
+        </ul>
+        <ul>AI
+          <li>Machine Learning</li>
+        </ul>
+        <ul>Framework
+          <li>Vue.js</li>
+        </ul>
       </span>
-
+      </div>
     </div>
   );
 }
